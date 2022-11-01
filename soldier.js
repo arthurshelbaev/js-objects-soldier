1acaeed79c67a5c0e3c69aa379ffcfbf967d18d4
arthurshelbaev <konodiobrandoda@mail.ru>
2022-11-01 16:35:45 +0300

    first commit

---
+++ soldier.js
@@ -0,0 +1,22 @@
+const soldier = {
+	name: 'Johnny',
+	health: 10,
+	gun: {
+		weapon: 'Ar-15',
+		bullets: 30
+	},
+	inventory: 3
+
+	shoot () {
+
+	},
+
+	reload () {
+
+	},
+
+	heat () {
+
+	},
+
+}
